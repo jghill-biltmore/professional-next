cb5db7b235a6d731bb1e99fc5d6fcffa68b209c5
jghill-bitlmore <jghill@biltmore.com>
2024-05-21 11:48:59 -0400

    [Video 17] Component Fundamentals

--- fancy-counter/src/Card.jsx
+++ fancy-counter/src/Card.jsx
@@ -7,7 +7,7 @@ export default function Card() {
   return (
     <div>
       <Title />
-      <Count />
+      <Count number={0} size="lg" />
       <ResetButton />
       <CountButtons />
     </div>
--- fancy-counter/src/Count.jsx
+++ fancy-counter/src/Count.jsx
@@ -1,3 +1,12 @@
-export default function Count() {
-  return <p>0</p>;
+function CountTitle() {
+  return <h2>Current count is:</h2>;
+}
+
+export default function Count({ number, size }) {
+  return (
+    <div>
+      <CountTitle />
+      <p>{number}</p>
+    </div>
+  );
 }
