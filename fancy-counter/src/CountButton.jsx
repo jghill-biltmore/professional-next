7b86714aaa891b64ad7ad69df281cbd4ffa96c0b
jghill-bitlmore <jghill@biltmore.com>
2024-05-21 16:58:43 -0400

    [Video 26] Finish Refactoring Reusable Button Component

--- fancy-counter/src/CountButton.jsx
+++ fancy-counter/src/CountButton.jsx
@@ -1,8 +1,14 @@
 import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 
 export default function CountButton({ setCount, type }) {
+  const handleClick = () =>
+    setCount((prev) => {
+      if (type === "minus") return prev - 1;
+      if (type === "plus") return prev + 1;
+    });
+
   return (
-    <button className="count-btn" onClick={() => setCount((prev) => prev + 1)}>
+    <button className="count-btn" onClick={handleClick}>
       {type === "minus" && <MinusIcon className="count-btn-icon" />}
       {type === "plus" && <PlusIcon className="count-btn-icon" />}
     </button>
