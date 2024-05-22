f37f234f70592b523a28f3aec9c6afac3c3cda38
jghill-bitlmore <jghill@biltmore.com>
2024-05-22 11:11:19 -0400

    [Video 35] Uncontrolled v Controlled (Textarea)

--- word-analytics/src/components/Textarea.jsx
+++ word-analytics/src/components/Textarea.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 export default function Textarea() {
+  const [text, setText] = useState("");
+
   return (
     <textarea
-      onChange={(e) => console.log(e.target.value)}
+      onChange={(e) => {
+        const newText = e.target.value;
+        setText(newText);
+      }}
       className="textarea"
       placeholder="Enter your text"
       spellCheck="false"
+      value={text}
     />
   );
 }
