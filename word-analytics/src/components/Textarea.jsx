936fb1c78c3f40170146572d5912f7de4963843a
jghill-bitlmore <jghill@biltmore.com>
2024-05-22 11:45:33 -0400

    [Video 39] Finish Validation (Remove Warning)

--- word-analytics/src/components/Textarea.jsx
+++ word-analytics/src/components/Textarea.jsx
@@ -5,15 +5,16 @@ export default function Textarea() {
   const [text, setText] = useState("");
   const [warning, setWarning] = useState("");
 
+  // Basic validation
   const handleChange = (e) => {
     let newText = e.target.value;
     if (newText.includes("<script>")) {
       newText = newText.replace("<script>", "");
       setWarning("No scripts tags allowed.");
-    } else if (newText.includes("@")) {
+    } else if (/@/.test(newText)) {
       newText = newText.replace("@", "");
       setWarning("No @ symbols allowed.");
-    }
+    } else setWarning("");
     setText(newText);
   };
 
