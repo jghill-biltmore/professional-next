039b286f3135a33d60b88f30d857c26f7d932c78
jghill-bitlmore <jghill@biltmore.com>
2024-05-22 11:28:48 -0400

    [Video 36] Basic Validation

--- word-analytics/src/components/Textarea.jsx
+++ word-analytics/src/components/Textarea.jsx
@@ -1,18 +1,28 @@
 import { useState } from "react";
+import Warning from "./Warning";
 
 export default function Textarea() {
   const [text, setText] = useState("");
+  const [showWarning, setShowWarning] = useState(false);
+
+  const handleChange = (e) => {
+    let newText = e.target.value;
+    if (newText.includes("<script>")) {
+      newText = newText.replace("<script>", "");
+      setShowWarning(true);
+    }
+    setText(newText);
+  };
 
   return (
-    <textarea
-      onChange={(e) => {
-        const newText = e.target.value;
-        setText(newText);
-      }}
-      className="textarea"
-      placeholder="Enter your text"
-      spellCheck="false"
-      value={text}
-    />
+    <div className="textarea">
+      <textarea
+        onChange={handleChange}
+        placeholder="Enter your text"
+        spellCheck="false"
+        value={text}
+      />
+      {showWarning && <Warning />}
+    </div>
   );
 }
