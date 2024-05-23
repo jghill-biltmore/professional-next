390a15d65f7425cc1168ae70b02dfac4b94ed395
jghill-bitlmore <jghill@biltmore.com>
2024-05-23 10:44:36 -0400

    [Video 46] Manipulate CSS In React

--- word-analytics/src/components/Stats.jsx
+++ word-analytics/src/components/Stats.jsx
@@ -3,7 +3,9 @@ import { H2 } from "./Headings";
 function Stat({ label, number = 0 }) {
   return (
     <section className="stat">
-      <span className="stat__number">{number}</span>
+      <span className={`stat__number ${number < 0 && "stat__number--limit"}`}>
+        {number}
+      </span>
       <H2>{label}</H2>
     </section>
   );
