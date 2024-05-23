cb98d943c4751e3538c0485f80e5f11eaa4e192f
jghill-bitlmore <jghill@biltmore.com>
2024-05-23 09:43:21 -0400

    [Video 40] Stats Section (JSX Markup)

--- word-analytics/src/components/Headings.jsx
+++ word-analytics/src/components/Headings.jsx
@@ -1,3 +1,7 @@
 export function H1({ children }) {
   return <h1 className="first-heading">{children}</h1>;
 }
+
+export function H2({ children }) {
+  return <h2 className="second-heading">{children}</h2>;
+}
--- word-analytics/src/components/Stats.jsx
+++ word-analytics/src/components/Stats.jsx
@@ -1,3 +1,24 @@
+import { H2 } from "./Headings";
+
 export default function Stats() {
-  return <section className="stats">Stats</section>;
+  return (
+    <section className="stats">
+      <section className="stat">
+        <span className="stat__number">0</span>
+        <H2>Words</H2>
+      </section>
+      <section className="stat">
+        <span className="stat__number">0</span>
+        <H2>Characters</H2>
+      </section>
+      <section className="stat">
+        <span className="stat__number">280</span>
+        <H2>Instagram</H2>
+      </section>
+      <section className="stat">
+        <span className="stat__number">2200</span>
+        <H2>Facebook</H2>
+      </section>
+    </section>
+  );
 }
